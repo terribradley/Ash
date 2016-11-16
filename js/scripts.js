1292531b94df174e4a9f73aaaf3ea39602e87e01
Terri Bradley <terribradley3@gmail.com>
2016-11-16 13:55:24 -0800

    header

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,13 @@
-var container = document.getElementById('header');
-var windowHeight = window.innerHeight;
-var windowWidth = window.innerWidth;
-var scrollArea = windowHeight;
-var one = document.getElementsByClassName('one');
-var two = document.getElementsByClassName('two');
-
-// update position of square 1 and square 2 when scroll event fires.
-window.addEventListener('scroll', function() {
-  var scrollTop = window.pageYOffset || window.scrollTop;
-  var scrollPercent = scrollTop/scrollArea || 0;
-
-  one.style.left = scrollPercent*window.innerWidth + 'px';
-  two.style.left = 800 - scrollPercent*window.innerWidth*0.6 + 'px';
-});
+// $(document).ready(function() {
+//   var lastScrollTop = 0;
+//   $("body").scroll(function (event) {
+//     var st = $(this).scrollTop();
+//     if (st > lastScrollTop) {
+//       $('one').animate({top: '-10%'}, 10);
+//     } else {
+//       $('one').animate({top: '10%'}, 10);
+//     }
+//     lastScrollTop = st;
+//   });
+//
+// });
