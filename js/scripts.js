b776530ea520e16eee219127c03a87729ebf0db3
Terri Bradley <terribradley3@gmail.com>
2016-11-18 16:19:45 -0800

    opening animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,39 @@
-// $(document).ready(function() {
-//   var lastScrollTop = 0;
-//   $("body").scroll(function (event) {
-//     var st = $(this).scrollTop();
-//     if (st > lastScrollTop) {
-//       $('one').animate({top: '-10%'}, 10);
-//     } else {
-//       $('one').animate({top: '10%'}, 10);
-//     }
-//     lastScrollTop = st;
-//   });
-//
-// });
+$(document).ready(function() {
+  var $win = $(window);
+  $win.on('scroll', function() {
+    var top = $win.scrollTop() * 2;
+     $('.one').css('transform', 'translateX(' + -top + 'px)');
+     $('.two').css('transform', 'translateY(' + top + 'px)');
+     $('.three').css('transform', 'translateY(' + -top + 'px)');
+     $('.four').css('transform', 'translateX(' + top + 'px)');
+     $('.pillar-top').css('transform', 'translateY(' + - top + 'px)');
+     $('.pillar-bottom').css('transform', 'translateY(' + top + 'px)');
+   });
+});
+
+
+
+
+
+
+
+
+
+
+    // event.preventDefault();
+    // var controller = new ScrollMagic.Controller();
+    //
+    // // var tween =TweenMax.to(".one", 5, {left:-100});
+    // // var tween2 = TweenMax.to(".two", 5, {top:1000});
+    // // var tween3 = TweenMax.to(".three", 5, {top:-300});
+    // // var tween4 = TweenMax.to(".four", 5, {left:1000});
+    // 	// build scene
+    // var scene = new ScrollMagic.Scene({
+    //   triggerElement: "#trigger1"
+    //   })
+    // 	.setClassToggle(".one", ".move")
+    //   .addIndicators({
+    //     name:'move terri',
+    //     colorTrigger: 'black'
+    //   })
+    // 	.addTo(controller);
